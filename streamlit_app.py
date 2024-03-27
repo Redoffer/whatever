f64843d56a28fd5e7e34a3e1bc088052aa99f244
Redoffer <145905245+Redoffer@users.noreply.github.com>
2024-03-27 02:08:34 +0000

    add

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,10 @@ import altair as alt
 st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
 st.title('📊 Interactive Data Explorer')
 
+st.title ("this is the app title")
+st.header("this is the markdown")
+st.markdown(" 'this is the header") 
+st.subheader("this is the subheader") 
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
